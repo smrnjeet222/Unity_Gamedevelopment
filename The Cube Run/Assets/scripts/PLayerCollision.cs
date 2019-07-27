622680794a061406a9230366dd4cd8812d5d0cc1
Simranjeet Singh <smrnjeet222@users.noreply.github.com>
2019-07-27 11:11:37 +0530

    The_Cube_Run-V2
    Smooth Movements and new Themes for each Level.

---
+++ The Cube Run/Assets/scripts/PLayerCollision.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Threading;
+
+public class PLayerCollision : MonoBehaviour{
+
+   public PLayer_movement movement ; 
+   
+   void OnCollisionEnter(Collision collisionInfo){
+       
+       if (collisionInfo.collider.tag == "Obstacle"){
+            movement.enabled = false;
+            FindObjectOfType<Manager>().Game_Over();
+
+       }
+
+   }
+  
+}
---
+++ The Cube Run/Assets/scripts/endtrigger.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class endtrigger : MonoBehaviour{
+    
+    public Manager gamemanager;
+    
+    
+    void OnTriggerEnter (){
+   
+        gamemanager.CompleteLevel();
+        
+    }
+    
+}
